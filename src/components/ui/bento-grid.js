7ed6c5918cd792010776974a9cc646e356902191
Aymane <aymanehilmi1@gmail.com>
2024-12-09 22:35:49 +0100

    Responsive

--- src/components/ui/bento-grid.js
+++ src/components/ui/bento-grid.js
@@ -7,7 +7,7 @@ export const BentoGrid = ({
     return (
         (<div
             className={cn(
-                "grid lg:auto-rows-[22rem] grid-cols-1 lg:grid-cols-3 gap-4 mx-auto lg:h-auto",
+                "grid lg:auto-rows-[22rem] grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto lg:h-auto",
                 className
             )}>
             {children}
