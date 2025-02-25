9a76f431ab14823431cfcb65bd980800c265d892
Aymane <aymane.hilmi@saint-gobain.com>
2025-02-25 13:59:14 +0100

    utils js commentaire pour ne pas oublier (from work pc)

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,7 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Optimisation de Tailwindcss à ameliorer pour les pages principales
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
 }
